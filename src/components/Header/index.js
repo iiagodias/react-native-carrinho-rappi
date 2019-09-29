e4204ed00452f12779ec56b376990d9f4905791b
Iago Dias <iago.unama@hotmail.com>
2019-09-28 22:53:29 -0300

    Intalação do Redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, TextInput, Text, ImageBackground } from 'react-native';
-import  styles from './styles';
+import styles from './styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Header extends Component {
@@ -8,29 +8,29 @@ export default class Header extends Component {
   render() {
 
     return (
-        <ImageBackground source={{uri: "https://www.freewebheaders.com/wp-content/gallery/food/various-spices-and-herbs-website-header.jpg"}} style={styles.background}>
-          <View style={styles.container}>
-            <View style={styles.boxIcone}>
-              <Icon style={styles.iconeVoltar} size={30} color="#FFFF" name="angle-left" />
-            </View>
+      <ImageBackground source={{ uri: "https://www.freewebheaders.com/wp-content/gallery/food/various-spices-and-herbs-website-header.jpg" }} style={styles.background}>
+        <View style={styles.container}>
+          <View style={styles.boxIcone}>
+            <Icon style={styles.iconeVoltar} size={30} color="#FFFF" name="angle-left" />
+          </View>
 
-            <View style={styles.boxBuscar}>
+          <View style={styles.boxBuscar}>
             <Icon style={styles.iconeBuscar} name="search" />
             <TextInput
               style={styles.input}
               placeholder="Buscar produtos"
               underlineColorAndroid="rgba(0,0,0,0.0)"
-              />
-            </View>
-
-            <View style={styles.boxIcone}>
-              <View style={styles.boxQuantidade}>
-                <Text style={styles.textQuantidade}>15</Text>
-              </View>
-              <Icon size={22} color="#FFFF" name="shopping-basket" />
-            </View>
+            />
           </View>
-        </ImageBackground>
+
+          <View style={styles.boxIcone}>
+            <View style={styles.boxQuantidade}>
+              <Text style={styles.textQuantidade}>15</Text>
+            </View>
+            <Icon size={22} color="#FFFF" name="shopping-basket" />
+          </View>
+        </View>
+      </ImageBackground>
     );
   }
 
